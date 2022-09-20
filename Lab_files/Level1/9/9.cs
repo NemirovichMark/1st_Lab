4680611fc1049704daf4e857cccca0bd1dd2fe4b
Andrey Kadomtsev <m2204942@edu.misis.ru>
2022-09-21 01:59:05 +0300

    l1n9

---
+++ Lab_files/Level1/9/9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Laboratory
+{
+    class Program
+    {
+        static int Factorial(int f)
+        {
+            if (f == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return f * Factorial(f-1); 
+            }
+        }
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double a = 1.00;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                
+                a = Math.Pow(-1, i) * Math.Pow(5, i) / Factorial(i);
+                sum += a;
+            }
+            Console.WriteLine($"Answer: {sum}");
+        }
+    }
+}
