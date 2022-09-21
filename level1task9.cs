d3ba50e7a7825d3816c55975a87514ca49f70316
Emil Gafarov <113677882+gafarovExe@users.noreply.github.com>
2022-09-21 16:29:20 +0300

    Update level1task9.cs
    change factorial function

--- level1task9.cs
+++ level1task9.cs
@@ -1,20 +1,19 @@
-    
-    
-using System;
-class HelloWorld {
-  static void Main() {
-    double sum=0;
-    double b=1;
-    for (int i=1; i<7; i++)
-    {
-        b=1;
-        for (int j=1; j<=i; j++)
-        {
-            b=b*j;
-        }
-        sum=sum+Math.Pow(-1,i)*Math.Pow(5,i)/b;
-    }
-    Console.WriteLine(sum);
-    }
-  }
 
+
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        double sum = 0;
+        double b = 1;
+        for (int i = 1; i < 7; i++)
+        {
+            b = b * i;
+            sum = sum + Math.Pow(-1, i) * Math.Pow(5, i) / b;
+        }
+        Console.WriteLine(sum);
+    }
+}
+
+//8,368055555555557
