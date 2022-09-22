423c5dd4dbe525876b0ab4b3c22dfe2173a490dd
Andrey Kadomtsev <m2204942@edu.misis.ru>
2022-09-22 21:51:19 +0300

    15and16

---
+++ Lab_files/Level1/15/15.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Laboratory
+{
+    class Program
+    {
+        static int numerator(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            if (n == 2)
+            {
+                return 2;
+            }
+            else
+            {
+                return (numerator(n-1) + numerator(n-2));
+            }
+        }
+
+        static double denominator(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            if (n == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return (denominator(n-1) + denominator(n-2));
+            }
+        }
+        static void Main(string[] args)
+        {
+            double answer = (numerator(5) / denominator(5));
+            Console.WriteLine($"Answer: {answer}");
+        }
+    }
+}
