44b870133ce0f850bd2b0cda4600944bbc86e5bc
Avenir <kadomcev2004@gmail.com>
2022-09-23 14:34:30 +0300

    L1N16

--- Lab_files/Level1/16/16.cs
+++ Lab_files/Level1/16/16.cs
@@ -6,7 +6,15 @@ namespace LaboratoryL1N16
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            ulong count = 1;
+            ulong last = 1;
+            for (int i = 2; i <= 64; i++)
+            {
+                last = last * 2;
+                count += last;
+            }
+
+            Console.WriteLine($"Seeds to be placed: {count}, weight: {count / 15}");
         }
     }
 }
