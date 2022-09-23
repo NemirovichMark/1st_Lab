541c79b1a7b3dded5124fad99b845e174a617cce
Avenir <kadomcev2004@gmail.com>
2022-09-23 14:07:05 +0300

    vs

--- Lab_files/Level3/#1/#1.cs
+++ Lab_files/Level3/#1/#1.cs
@@ -18,7 +18,7 @@ namespace LaboratoryL3N1
         static void Main(string[] args)
         {
             int i = 0;
-            double a = 0.1, b = 1, h = 0.1;
+            double a = 0.1, h = 0.1;
             double x = a, sigma = 0;
             double s;
             s = (((Math.Pow(-1.00, i)) * Math.Pow(x, (2*i))) / Factorial(2*i));
