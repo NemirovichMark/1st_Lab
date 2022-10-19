173dbfde36e4fa8ade24eff9c9958456b437e3a9
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-10-19 16:02:28 +0300

    Update Level3_Task8.cs

--- Level3_Task8.cs
+++ Level3_Task8.cs
@@ -12,23 +12,21 @@ namespace SoloLearn
 		static void Main(string[] args)
 		{
 			double a=0.1, b=1, h=0.05;
-            double sum = 0, meat = 1, y=0;
+            double sum = 0, meat = 1, y=0, fact=1;
             for(double x=a;x<=b;x+=h){
                 sum=0;
                 meat=1;
                 y=0;
+		fact=1;
                 for(int i=0;abs(meat)>=0.0001;i++){
-                    meat=Math.Pow(2*x,i)/fact(i);
+                    meat=Math.Pow(2*x,i)/fact;
                     sum+=meat;
+		    fact*=(fact+1);
                 }
                 y=Math.Pow(Math.E,2*x);
                 Console.WriteLine($"X: {x}, S: {sum}, Y: {y}");
             }
 		}
-        static int fact(int n){
-            if(n<=1) return 1;
-            return n*fact(n-1);
-        }
         static double abs(double n){
             if(n>0) return n;
             return -1*n;
