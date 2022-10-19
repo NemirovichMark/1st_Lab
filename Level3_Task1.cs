8970185af4f225c66c0ba21c2ee647e24b7e7f51
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-10-19 16:05:45 +0300

    Update Level3_Task1.cs

--- Level3_Task1.cs
+++ Level3_Task1.cs
@@ -13,11 +13,12 @@ namespace Sololearn
         {
             double a=0.1, b=1, h=0.1;
             double sum = 0, meat = 1, y=0;
+            int fact = 1;
             for(double x=a;x<=b;x+=h){
                 sum=0;
                 meat=1;
                 y=0;
-                int fact=1;
+                fact=1;
                 for(int i=0;abs(meat)>=0.0001;i++){
                     meat=Math.Pow(-1,i)*Math.Pow(x,2*i)/fact;
                     fact*=(2*i-1)*2*i;
