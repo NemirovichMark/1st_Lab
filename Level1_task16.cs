304e2bd2c8b2e2c88e11fc79b1c0f05b9659157d
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-10-28 12:26:43 +0400

    Update Level1_task16.cs

--- Level1_task16.cs
+++ Level1_task16.cs
@@ -11,19 +11,13 @@ namespace Sololearn
     {
         static void Main(string[] args)
         {
-            decimal s=0;
+            decimal s=0, pow=1;
             for(int i=0; i<64; i++){
-                s+=Pow2To(i);
+                s+=pow;
+                pow*=2;
             }
             Console.WriteLine("Result is: {0}",s);
             Console.WriteLine("Result obtained with 100% accuracy");
-        }
-        
-        //Method, that returns 2^a
-        static decimal Pow2To(int a){
-            if(a==0) return 1m;
-            if(a%2==0) return Pow2To(a/2)*Pow2To(a/2);
-            return 2m*Pow2To(a-1);
         }
     }
 }
